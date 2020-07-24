9e9ebb23ae3ba05064f8d2923a24f5cf57d83c00
Somya Kumari <somyapoonam.k@amail.com>
2020-07-24 12:55:41 +0530

    removed unused code

--- src/main/java/com/cycle/repository/BicycleRepository.java
+++
@@ -1,9 +0,0 @@
-/*package com.cycle.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cycle.model.Bicycle;
-public interface BicycleRepository extends JpaRepository<Bicycle,Integer> {
-
-	
-
-}*/
--- src/main/java/com/cycle/service/BicycleService.java
+++
@@ -1,8 +0,0 @@
-package com.cycle.service;
-
-import com.cycle.model.Bicycle;
-
-public interface BicycleService {
-	
-	public Bicycle getBicyclePrice(Integer bicycleId, double updatedPrice);
-}
